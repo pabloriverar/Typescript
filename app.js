79ecdae7c50ec62aa077008b8e56124d48efe80b
Pablo Rivera <pabloriverar.sv@gmail.com>
2020-03-05 10:13:10 -0600

    jueves 05 de marzo

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
 "use strict";
 (function () {
-    function activar(quien, objeto, momento) {
-        if (objeto === void 0) { objeto = 'batiseñal'; }
-        if (momento) {
-            console.log(quien + " activo la " + objeto + " en la " + momento + ".");
-        }
-        else {
-            console.log(quien + " activo la " + objeto + ".");
-        }
-    }
-    activar('Gordon', 'batiseñal', 'tarde');
 })();
--- app.ts
+++ app.ts
@@ -1,15 +1,7 @@
 
-(function(){
+(() => {
 
-   function activar (quien: string, objeto:string = 'batiseñal',momento?:string){
 
-    if(momento){    
-    console.log(`${quien} activo la ${objeto} en la ${momento}.`);
-    }else{
-        console.log(`${quien} activo la ${objeto}.`);  
-    }
-   }
 
-   activar('Gordon','batiseñal','tarde');
 
 })();

818481c0f5beeddf8e5b2a5a9905dc5e7bcf8b80
Pablo Rivera <pabloriverar.sv@gmail.com>
2020-03-05 16:16:05 -0600

    salvando el ultimo avance del jueves

---
+++ typescript/Interface.ts
@@ -0,0 +1,27 @@
+
+(() => {
+
+    interface Xmen{
+        nombre:string;
+        edad: number;
+        poder?:string;
+    }
+
+    const enviarMision = (xmen:Xmen)=> {
+
+        console.log(`Enviando a ${xmen.nombre} a la mision`);
+    }
+
+    const regresarAlCuartel = (xmen:Xmen ) => {
+
+        console.log(`regresando a ${xmen.nombre} de la mision`);
+    }
+
+    const wolverine: Xmen = {
+     nombre:'Logan', 
+     edad: 60  
+    }
+
+    enviarMision(wolverine);
+    regresarAlCuartel(wolverine);
+})();
